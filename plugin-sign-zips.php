89a4e63e978a35d37161a17ed1d5ab06390918e0
Dion Hulse <contact@dd32.id.au>
2019-01-22 19:24:14 +1100

    Initial POC versions of these plugins.

---
+++ plugin-sign-zips.php
@@ -0,0 +1,121 @@
+<?php
+/*
+ * Plugin Name: WordPress Signing: Sign w.org ZIPs
+ * Plugin URI: https://github.com/dd32/wp-signed-updates
+ * Description: This plugin performs client-side signing of WordPress-served ZIP files, it's intended to allow local development, and test the process of generating signatures.
+ * Author: Dion Hulse
+ * Version: 0.1
+ * Author URI: https://dd32.id.au/
+ */
+
+class WP_Signing_Signer {
+	protected $downloaded_files = [];
+	protected const VALID_DOMAINS = [ 'wordpress.org', 'downloads.wordpress.org', 's.w.org' ];
+
+	public function __construct() {
+		// Validate we have the Sodium PHP extension available.
+		if ( ! function_exists( 'sodium_crypto_sign_verify_detached' ) ) {
+			return;
+		}
+
+		add_filter( 'wp_trusted_keys',  [ $this, 'wp_trusted_keys' ] );
+		add_filter( 'pre_http_request', [ $this, 'intercept_signature_http_request' ], 10, 3 );
+		add_filter( 'http_response',    [ $this, 'remember_requested_files' ], 10, 3 );
+	}
+
+	public function intercept_signature_http_request( $filter_value, $args, $url ) {
+		// Only sign specific URLs
+		$hostname = parse_url( $url, PHP_URL_HOST );
+		if ( ! in_array( $hostname, self::VALID_DOMAINS, true ) ) {
+			return $filter_value;
+		}
+
+		// Only intercept requests for file signatures
+		if ( ! preg_match( '!^(.+)\.sig$!i', $url, $matches ) ) {
+			return $filter_value;
+		}
+		$file_url = $matches[1];
+
+		if ( ! isset( $this->downloaded_files[ $file_url ] ) ) {
+			return $filter_value;
+		}
+
+		$signature = $this->sign_file( $this->downloaded_files[ $file_url ] );
+
+		// Abort.
+		if ( ! $signature ) {
+			return $filter_value;
+		}
+
+		// Intercept the request, return our new signature.
+		return [
+			'body' => $signature,
+			'response' => [
+				'code' => 200,
+				'message' => 'OK',
+			],
+			'headers' => [],
+			'cookies' => [],
+			'filename' => null,
+			'http_rsponse' => false,
+		];
+	}
+
+	// Remember the downloaded files to allow us to use the local file for signing.
+	public function remember_requested_files( $response, $args, $url ) {
+		if ( $response['filename'] ) {
+			$this->downloaded_files[ $url ] = $response['filename'];
+		}
+
+		return $response;
+	}
+
+	// Add our public key to WordPress.
+	public function wp_trusted_keys( $keys ) {
+		if ( ! get_option( 'signing_signer_public_key' ) ) {
+			$this->regenerate_keys();
+		}
+
+		if ( $public_key = get_option( 'signing_signer_public_key' ) ) {
+			$keys[] = $public_key;
+		}
+
+		return $keys;
+	}
+
+	// Sign a file.
+	public function sign_file( $filename ) {
+		if ( ! file_exists( $filename ) ) {
+			return false;
+		}
+		$file_contents = file_get_contents( $filename );
+		if ( ! $file_contents ) {
+			return false;
+		}
+
+		$secret_key = get_option( 'signing_signer_secret_key' );
+		if ( ! $secret_key ) {
+			$this->regenerate_keys();
+			$secret_key = get_option( 'signing_signer_secret_key' );
+		}
+		$secret_key = hex2bin( $secret_key );
+
+		$signature = sodium_crypto_sign_detached( $file_contents, $secret_key );
+
+		return bin2hex( $signature );
+	}
+
+	// Generate a keypair for signing, not stored securely.
+	public function regenerate_keys() {
+		$keypair = sodium_crypto_sign_keypair();
+
+		$secret_key = sodium_crypto_sign_secretkey( $keypair );
+		$public_key = sodium_crypto_sign_publickey( $keypair );
+
+		update_option( 'signing_signer_secret_key', bin2hex( $secret_key ) );
+		update_option( 'signing_signer_public_key', bin2hex( $public_key ) );
+
+		return true;
+	}
+}
+new WP_Signing_Signer();
---
+++ plugin-verify-signatures.php
@@ -0,0 +1,100 @@
+<?php
+/*
+ * Plugin Name: WordPress Signing: Verify Zips
+ * Plugin URI: https://github.com/dd32/wp-signed-updates
+ * Description: This plugin performs client-side validation of WordPress.org ZIPs bsed on public keys.
+ * Author: Dion Hulse
+ * Version: 0.1
+ * Author URI: https://dd32.id.au/
+ */
+
+class WP_Signing_Verify {
+
+	protected const TRUSTED_KEYS = [];
+	protected const VALID_DOMAINS = [ 'wordpress.org', 'downloads.wordpress.org', 's.w.org' ];
+
+	public function __construct() {
+		// Validate we have the Sodium PHP extension available.
+		if ( ! function_exists( 'sodium_crypto_sign_verify_detached' ) ) {
+			return;
+		}
+
+		add_action( 'upgrader_pre_download', [ $this, 'download_package_override' ], 1, 3 );
+	}
+
+	public function validate_signature( $download_file, $signature_url ) {
+		$trusted_keys = apply_filters( 'wp_trusted_keys', self::TRUSTED_KEYS );
+
+		// Fetch the signature of the file.
+		// We're using detached signatures here, where it's stored in a separate file.
+		$signature_request = wp_safe_remote_get( $signature_url );
+		$signature = wp_remote_retrieve_body( $signature_request );
+		if ( is_wp_error( $signature_request ) || ! $signature ) {
+			return false;
+		}
+
+		$file_contents = file_get_contents( $download_file );
+
+		foreach ( $trusted_keys as $key ) {
+			if ( sodium_crypto_sign_verify_detached( hex2bin( $signature ), $file_contents, hex2bin( $key ) ) ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	// Override the WP_Upgrader download_package() method to perform signature verification.
+	public function download_package_override( $filter_value, $package, $upgrader ) {
+		if ( ! preg_match( '!^(http|https)://!i', $package ) ) {
+			return $filter_value;
+		}
+
+		// Only sign specific URLs
+		$hostname = parse_url( $package, PHP_URL_HOST );
+		if ( ! in_array( $hostname, self::VALID_DOMAINS, true ) ) {
+			return $filter_value;
+		}
+
+		if ( empty( $package ) ) {
+			return new WP_Error( 'no_package', $upgrader->strings['no_package'] );
+		}
+
+		$upgrader->skin->feedback( 'downloading_package', $package );
+
+		$download_file = download_url( $package );
+
+		// All signatures are available at `$url.sig`.
+		$signature_url = "$package.sig";
+
+		// Verify the Signature
+		$upgrader->skin->feedback(
+			'Verifying file signature&#8230; Fetching %s&#8230;',
+			'<code>' . $signature_url . '</code>'
+		);
+
+		if ( ! $this->validate_signature( $download_file, $signature_url ) ) {
+			@unlink( $download_file );
+			$upgrader->skin->feedback( '<strong>' . 'Signature Validation Failed.' . '</strong>' );
+			return new WP_Error(
+				'signature_failure',
+				sprintf(
+					'The signature validation of %s failed.',
+					'<code>' . basename( $package ) . '</code>'
+				)
+			);
+		}
+
+		$upgrader->skin->feedback(
+			'<strong>' . 'Signature Verification of %s Passed.' . '</strong>',
+			'<code>' . basename( $package ) . '</code>'
+		);
+
+		if ( is_wp_error( $download_file ) ) {
+			return new WP_Error( 'download_failed', $upgrader->strings['download_failed'], $download_file->get_error_message() );
+		}
+
+		return $download_file;
+	}
+}
+new WP_Signing_Verify();
