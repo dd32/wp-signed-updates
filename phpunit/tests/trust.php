d555a3ca36edda622a08cbe05e3f7a0cbfc34b9d
Dion Hulse (dd32) <contact@dd32.id.au>
2020-07-08 17:33:52 +1000

    Use the key context for trust rather than cert.

--- phpunit/tests/trust.php
+++ phpunit/tests/trust.php
@@ -8,9 +8,9 @@ class Test_Trust extends WP_Signing_UnitTestCase {
 		$root_one = file_get_contents( SIGNING_PLUGIN_DIR . '/keys/root.pub' );
 		$root_two = file_get_contents( SIGNING_PLUGIN_DIR . '/keys/root2.pub' );
 
-		$this->assertTrue( Plugin::instance()->is_trusted( $root_one, 'cert' ) );
-		$this->assertTrue( Plugin::instance()->is_trusted( $root_two, 'cert' ) );
-		$this->assertFalse( Plugin::instance()->is_trusted( $root_one . $root_two, 'cert' ) );
+		$this->assertTrue( Plugin::instance()->is_trusted( $root_one, 'key' ) );
+		$this->assertTrue( Plugin::instance()->is_trusted( $root_two, 'key' ) );
+		$this->assertFalse( Plugin::instance()->is_trusted( $root_one . $root_two, 'key' ) );
 	}
 	function test_root_is_not_trusted_to_sign_responses() {
 		$key = file_get_contents( SIGNING_PLUGIN_DIR . '/keys/root.pub' );
@@ -29,7 +29,7 @@ class Test_Trust extends WP_Signing_UnitTestCase {
 	function test_intermediate_is_trusted() {
 		$key = file_get_contents( SIGNING_PLUGIN_DIR . '/keys/intermediate.pub' );
 
-		$this->assertTrue( Plugin::instance()->is_trusted( $key, 'cert' ) );
+		$this->assertTrue( Plugin::instance()->is_trusted( $key, 'key' ) );
 	}
 
 	function test_intermediate_is_not_trusted_to_sign_responses() {
@@ -47,6 +47,6 @@ class Test_Trust extends WP_Signing_UnitTestCase {
 	function test_api_key_is_not_trusted_to_sign_keys() {
 		$key = file_get_contents( SIGNING_PLUGIN_DIR . '/keys/api.pub' );
 
-		$this->assertTrue( Plugin::instance()->is_trusted( $key, 'cert' ) );
+		$this->assertTrue( Plugin::instance()->is_trusted( $key, 'key' ) );
 	}
 }
