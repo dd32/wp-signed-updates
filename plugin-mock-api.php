e951311786d132b1e7ad55668669044de10e0e01
Dion Hulse (dd32) <contact@dd32.id.au>
2020-07-08 15:25:27 +1000

    Take two, 2020, starting from scratch.

--- plugin-mock-api.php
+++ plugin-mock-api.php
@@ -1,9 +1,9 @@
 <?php
-namespace dd32\WordPressSignedUpdates;
+namespace dd32\WordPressSignedUpdates\MockAPI;
 /*
- * Plugin Name: WordPress Signing, 2020
+ * Plugin Name: WordPress Signing, 2020, Mock API.
  * Plugin URI: https://github.com/dd32/wp-signed-updates
- * Description: This plugin does stuff. Client-side stuff.
+ * Description: This plugin does stuff, mocking API.w.org
  * Author: Dion Hulse
  * Version: 0.2
  * Author URI: https://dd32.id.au/
