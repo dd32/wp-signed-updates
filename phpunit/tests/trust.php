ee3b00fa20587d3b3a1ef88ad08fec0adfde2907
Dion Hulse (dd32) <contact@dd32.id.au>
2020-07-09 19:55:59 +1000

    Fix test function names

--- phpunit/tests/trust.php
+++ phpunit/tests/trust.php
@@ -7,24 +7,25 @@ class Test_Trust extends WP_Signing_UnitTestCase {
 	function test_invalid_context() {
 		$key = file_get_contents( SIGNING_PLUGIN_DIR . '/keys/api.pub' );
 
-		$this->assertFalse( Plugin::instance()->is_trusted( $key, 'unknown-context' ) );
+		$this->assertTrue( Plugin::instance()->is_trusted( $key ) );
+		$this->assertFalse( Plugin::instance()->key_is_valid_for( $key, 'unknown-context' ) );
 	}
 
 	function test_key_expiry() {
 		$key = file_get_contents( SIGNING_PLUGIN_DIR . '/keys/expired-intermediate.pub' );
 
 		$this->assertTrue( Plugin::instance()->is_trusted( $key ) );
-		$this->assertTrue( Plugin::instance()->is_trusted_for( $key, 'key' ) );
-		$this->assertTrue( Plugin::instance()->is_trusted_for_date( $key, '2020-03-01T00:00:00Z' ) );
-		$this->assertFalse( Plugin::instance()->is_trusted_for_date( $key, '2020-09-01T00:00:00Z' ) );
+		$this->assertTrue( Plugin::instance()->key_is_valid_for( $key, 'key' ) );
+		$this->assertTrue( Plugin::instance()->key_is_valid_for_date( $key, '2020-03-01T00:00:00Z' ) );
+		$this->assertFalse( Plugin::instance()->key_is_valid_for_date( $key, '2020-09-01T00:00:00Z' ) );
 	}
 
 	function test_key_expiry_invalid_key() {
 		$key = file_get_contents( SIGNING_PLUGIN_DIR . '/keys/invalid-api.pub' );
 
 		$this->assertFalse( Plugin::instance()->is_trusted( $key ) );
-		$this->assertFalse( Plugin::instance()->is_trusted_for( $key, 'api' ) );
-		$this->assertFalse( Plugin::instance()->is_trusted_for_date( $key, '2025-01-01T00:00:00Z' ) );
+		$this->assertFalse( Plugin::instance()->key_is_valid_for( $key, 'api' ) );
+		$this->assertFalse( Plugin::instance()->key_is_valid_for_date( $key, '2025-01-01T00:00:00Z' ) );
 	}
 
 	/**
@@ -36,10 +37,10 @@ class Test_Trust extends WP_Signing_UnitTestCase {
 		$this->assertTrue( Plugin::instance()->is_trusted( $key ) );
 
 		foreach ( $should_be_true as $i ) {
-			$this->assertTrue( Plugin::instance()->is_trusted_for( $key, $i ) );
+			$this->assertTrue( Plugin::instance()->key_is_valid_for( $key, $i ) );
 		}
 		foreach ( $should_be_false as $i ) {
-			$this->assertFalse( Plugin::instance()->is_trusted_for( $key, $i ) );
+			$this->assertFalse( Plugin::instance()->key_is_valid_for( $key, $i ) );
 		}
 	}
 
