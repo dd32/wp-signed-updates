ef2e9e17e4b0220c3558ebea8fe867538cd4408b
Dion Hulse (dd32) <contact@dd32.id.au>
2020-07-09 16:19:36 +1000

    Add Packages key, unit test that tests all keys based on their json manifest

--- phpunit/tests/trust.php
+++ phpunit/tests/trust.php
@@ -49,4 +49,46 @@ class Test_Trust extends WP_Signing_UnitTestCase {
 
 		$this->assertFalse( Plugin::instance()->is_trusted( $key, 'key' ) );
 	}
+
+	function test_packages_key_is_trusted_to_sign_zips() {
+		$key = file_get_contents( SIGNING_PLUGIN_DIR . '/keys/packages.pub' );
+
+		$this->assertTrue( Plugin::instance()->is_trusted( $key, 'core' ) );
+		$this->assertTrue( Plugin::instance()->is_trusted( $key, 'api' ) );
+		$this->assertTrue( Plugin::instance()->is_trusted( $key, 'api' ) );
+	}
+
+	/**
+	 * @dataProvider data_find_all_key_manifest_matrix
+	 */
+	function test_keys_from_manifest( $key, $should_be_true, $should_be_false ) {
+		foreach ( $should_be_true as $i ) {
+			$this->assertTrue( Plugin::instance()->is_trusted( $key, $i ) );
+		}
+		foreach ( $should_be_false as $i ) {
+			$this->assertFalse( Plugin::instance()->is_trusted( $key, $i ) );
+		}
+	}
+
+	function data_find_all_key_manifest_matrix() {
+		$known_types = [ 'key', 'api', 'core', 'plugins', 'themes', 'translations' ];
+
+		$return = [];
+
+		foreach ( glob( SIGNING_PLUGIN_DIR . '/keys/*.json' ) as $json_file ) {
+			$base = explode( '.', $file )[0];
+
+			$json = json_decode( file_get_contents( $base . '.json' ), true );
+			$pub_key = file_get_contents( $base . '.pub' );
+
+			$return[] = [
+				$pub_key,
+				$json['canSign'],
+				array_diff( $known_types, $json['canSign'] )
+			];
+
+		}
+
+		return $return;
+	}
 }
